ee8f9c9c3095029848f54389eb5bbb69f43f1aeb
TheoSimoes <theo.simoes@hotmail.fr>
2022-11-08 13:38:00 +0100

    e

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("hehe on fait du js noraj");
   res.render('index', { title: 'Express' });
 });
 
