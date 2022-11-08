2b847514804fa5bd2db116b55ec569e7c507be76
TheoSimoes <theo.simoes@hotmail.fr>
2022-11-08 16:33:25 +0100

    init app not working

---
+++ watwep/app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react'
+// import {SearchBar} from './searchbar/searchbar'
+
+function App() {
+  return (<></>
+    // <SearchBar></SearchBar>
+  );
+}
+
+export default App;
